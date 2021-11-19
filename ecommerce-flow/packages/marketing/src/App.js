8b751e7f8f30feba589661049207b29ffbee0900
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2021-11-18 21:51:22 -0300

    feat: initial marketing component with dummy elements

---
+++ ecommerce-flow/packages/marketing/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { StylesProvider } from '@material-ui/core/styles';
+
+import Landing from './components/Landing';
+import Pricing from './components/Pricing';
+
+const App = () => {
+  return (
+    <div>
+      <StylesProvider>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route path="/pricing" component={Pricing} />
+          </Switch>
+        </BrowserRouter>
+      </StylesProvider>
+    </div>
+  );
+};
+
+export default App;
