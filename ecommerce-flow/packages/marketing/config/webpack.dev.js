22f363c2d4fe284989ad667c7954b5541e32780d
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2021-11-20 13:43:39 -0300

    feat: add shared dependencies between projects

--- ecommerce-flow/packages/marketing/config/webpack.dev.js
+++ ecommerce-flow/packages/marketing/config/webpack.dev.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
 
 const devConfig = {
   mode: 'development',
@@ -19,6 +20,7 @@ const devConfig = {
       exposes: {
         './MarketingApp': './src/bootstrap',
       },
+      shared: packageJson.dependencies,
     }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
