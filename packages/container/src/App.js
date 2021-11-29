848f99953cda906018a1c666ededc4614dd6ed25
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2021-11-29 08:51:51 -0300

    feat: add Header component

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
+import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
 
 const App = () => {
   return (
-    <>
-      <h1>Hi there!</h1>
-      <hr />
+    <BrowserRouter>
+      <Header />
       <MarketingApp />
-    </>
+    </BrowserRouter>
   );
 };
 
