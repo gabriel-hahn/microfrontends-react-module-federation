04c246e79bc4718ffe7939364e9d79a42e3cd35a
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2021-12-17 00:12:55 -0300

    chore: fix marketing filename

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -13,7 +13,7 @@ const prodConfig = {
   plugins: [
     new ModuleFederationPlugin({
       name: 'marketing',
-      filename: 'marketing/remoteEntry.js',
+      filename: 'remoteEntry.js',
       exposes: {
         './MarketingApp': './src/bootstrap',
       },
