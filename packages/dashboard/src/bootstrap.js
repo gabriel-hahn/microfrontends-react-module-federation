ab05699a86bb5b8976f35ed502299a768511bbff
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2021-12-22 13:30:19 -0300

    feat: add initial dashboard components

---
+++ packages/dashboard/src/bootstrap.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
+
+import App from './App';
+
+const mount = (element, { onNavigate, defaultHistory, initialPath }) => {
+  const history =
+    defaultHistory ||
+    createMemoryHistory({
+      initialEntries: [initialPath],
+    });
+
+  !!onNavigate && history.listen(onNavigate);
+
+  ReactDOM.render(<App history={history} />, element);
+
+  return {
+    onParentNavigate({ pathname: nextPathName }) {
+      if (history.location.pathname !== nextPathName) {
+        history.push(nextPathName);
+      }
+    },
+  };
+};
+
+if (process.env.NODE_ENV === 'development') {
+  const devRootElement = document.querySelector('#_dashboard-dev-root');
+
+  if (devRootElement) {
+    mount(devRootElement, { defaultHistory: createBrowserHistory() });
+  }
+}
+
+export { mount };
