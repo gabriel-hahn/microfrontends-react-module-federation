dcc4ae4917a6d2657004bbd6e263b7c06fea7633
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2021-12-23 10:18:59 -0300

    fix: Dashboard app import

--- packages/container/src/components/DashboardApp.js
+++ packages/container/src/components/DashboardApp.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import { mount } from 'auth/DashboardApp';
+import { mount } from 'dashboard/DashboardApp';
 
 const DashboardApp = () => {
   const ref = useRef(null);
